852a546aa3c84a18bca8c2785b8085627f7795a8
Likhon <likhon.929@gmail.com>
2025-02-04 17:02:49 +0600

    Add data library for file system operations and update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 /*
-* Title: Uptime Monitoring Application
-* Description: A RESTful API to monitor up or down time of user defined links
-* Author: Jubayer Alam Likhon
-* Date: 03/02/2024
-*/
+ * Title: Uptime Monitoring Application
+ * Description: A RESTful API to monitor up or down time of user defined links
+ * Author: Jubayer Alam Likhon
+ * Date: 03/02/2024
+ */
 
 // Dependencies
-const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
+const http = require("http");
+const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
+const data = require("./lib/data");
 
 // app object - module scaffolding
 const app = {};
+
+// Testing file system
+// @TODO: Testing file system
+data.delete("test", "newFile", (err) => {
+  console.log(err);
+});
 
 // Configuration
 
